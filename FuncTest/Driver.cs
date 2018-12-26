fe781f9d41ea1eca7f549d8b0c3943214ad775d5
Gorf963 <James.Wiegand@verizon.net>
2018-12-26 17:22:20 -0500

    function and operation testing

---
+++ FuncTest/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.FuncTest
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                HelloFunTest.Run(qsim).Wait();
+                var tempInt = AddInt.Run(qsim, 3, 3).Result;
+                Console.WriteLine("AddInt"+tempInt);
+                var tempDouble = ProductNumber.Run(qsim, 3, 3).Result;
+                Console.WriteLine("ProductDouble" + tempDouble);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FuncTest/Operations.qs
@@ -0,0 +1,22 @@
+﻿namespace Quantum.FuncTest
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+    
+    operation HelloFunTest () : Unit {
+        Message("Hello quantum!");
+    }
+	operation AddInt(a: Int, b: Int): Int {
+		body {
+			mutable c = 0;
+			set c = a + b;
+			return (c);
+		}
+	}
+	function ProductNumber(a: Double, b:Double):Double {
+		mutable c = 0.0;
+		set c = a * b;
+		return (c);
+	}
+	
+}
