a97c806fdfa7bfe5cd605d4eb3e2082c5f841fb8
Gorf963 <James.Wiegand@verizon.net>
2018-12-31 14:38:49 -0500

    Belltest added

---
+++ BellTest/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.BellTest
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                Result inital = new Result();
+                inital = Result.Zero;
+                var res = BellTest.Run(qsim, 1000, inital).Result;
+                Console.WriteLine("0: {0} 1: {1}",res.Item1,   res.Item2);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
